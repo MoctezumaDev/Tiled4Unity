d931725cd040ae5a7495e290f7ea55ec4e5ce708
Leon Moctezuma <leon.moctezuma@gmail.com>
2016-12-24 05:41:10 +0100

    Add example files and scenes

--- Assets/Examples/PinkDot/Player.cs
+++ Assets/Examples/PinkDot/Player.cs
@@ -7,12 +7,17 @@ public class Player : MonoBehaviour {
     [SerializeField]
     float _maxVelocity = 10.0f;
 
+    private float _xMultiplier = 1.0f;
+    private float _yMultiplier = 1.0f;
+
     Rigidbody2D _rigidbody;
 
 	// Use this for initialization
 	void Awake () {
         _rigidbody = GetComponent<Rigidbody2D>();
-    }
+	    _xMultiplier = transform.localScale.x;
+	    _yMultiplier = transform.localScale.y;
+	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -23,29 +28,33 @@ public class Player : MonoBehaviour {
 
             if (_rigidbody.velocity.y == 0)
             {
-                _rigidbody.velocity += upVelocity;
+                _rigidbody.velocity += upVelocity * _yMultiplier;
             }
         }
 
         if(Input.GetKey(KeyCode.RightArrow))
         {
             Vector2 rightVelocity = new Vector2(_maxVelocity, 0);
-            _rigidbody.velocity += rightVelocity;
+            _rigidbody.velocity += rightVelocity * _xMultiplier;
         }
 
         if(Input.GetKey(KeyCode.LeftArrow))
         {
             Vector2 leftVelocity = new Vector2(-_maxVelocity, 0);
-            _rigidbody.velocity += leftVelocity;
+            _rigidbody.velocity += leftVelocity * _xMultiplier;
         }
 
-        if(!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
-        {
-            if (_rigidbody.velocity.y == 0)
-            {
+	    if (!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+	    {
+	        if (_rigidbody.velocity.y.Equals(0.0f))
+	        {
+	            _rigidbody.angularVelocity = 0;
+	            _rigidbody.drag = 0.5f * _xMultiplier;
+	        }
+	        else
+	        {
                 _rigidbody.angularVelocity = 0;
-                //_rigidbody.velocity = Vector2.zero;
-                _rigidbody.drag = 0.5f;
+                _rigidbody.drag = 0;
             }
         }
 
@@ -58,14 +67,14 @@ public class Player : MonoBehaviour {
         float horizontalVelocity = velocity.x;
         float verticalVelocity = velocity.y;
 
-        if ( Mathf.Abs(horizontalVelocity) > _maxVelocity)
+        if ( Mathf.Abs(horizontalVelocity) > _maxVelocity * _xMultiplier)
         {
-            horizontalVelocity = Mathf.Sign(velocity.x) * _maxVelocity;
+            horizontalVelocity = Mathf.Sign(velocity.x) * _maxVelocity * _xMultiplier;
         }
 
-        if ( verticalVelocity > _maxVelocity )
+        if ( verticalVelocity > _maxVelocity * _yMultiplier )
         {
-            verticalVelocity = _maxVelocity;
+            verticalVelocity = _maxVelocity * _yMultiplier;
         }
 
         _rigidbody.velocity = new Vector2(horizontalVelocity, verticalVelocity);
